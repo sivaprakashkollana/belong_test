92b5fcbb20f9e43725ff66fe9ef517ee46e01aec
siva <siva@compile.com>
2016-12-25 23:52:48 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-# belong_test
+Instruction Before Usage:
+-----
 Following are the steps to use this package
 cd belong_test/ # you can find requirements.txt here
 1) sudo apt-get install redis-tools, redis-server
 2) pip install -r requirements.txt
 3) ./start.sh # if it fails here run 'chmod a+x start.sh'
 
-
+-----
 ```python # Please have a look at worker_app/tests.py for more information
 import requests
 server = '127.0.0.1:8000' # default server
 submit_job_url = 'http://%s/client/calculator/' % server
 fetch_status_url = 'http://%s/client/job_status/'% server
+```
 # To submit a job
+```
 data = {'k1':val1,'k2':val2}
 req = requests.post(submit_job_url, data=data)
-
+```
 # To query for a job
+```
 data = {'id':job_id}
 job_result = requests.post(fetch_status_url, data=jdata)
 
 ```
-
-
--- belong_test/custom_queue/utils.py
+++ belong_test/custom_queue/utils.py
@@ -57,3 +57,68 @@ class SimpleQueueFromRedis(object):
     def put(self, job_id):
         self.redis_object.rpush(self.redis_key, job_id)
 
+
+'''
+Using only Redis for storing, data and to generate unique keys, db was not involved, dropped this, cos it didn't function with multiconsumer
+import time
+import threading
+from client.models import InputJobs
+
+PROCESSED = 1
+PERIODIC_REFRESH_CUTOFF=120 # seconds
+BATCH_SIZE=50
+
+class SimpleQueueFromRedis(object):
+    def __init__(self, redis_object):
+        self.redis_object=redis_object
+        self.un_processed_items=[]
+        self.last_refresh=time.time()
+        self.redis_key='jobs'
+        self.__init_data()
+        self._check_lock=threading.Lock()
+
+    def __init_data(self):
+        self.redis_object.delete(self.redis_key)
+        self.refresh_unprocessed()
+
+    def periodic_refresh(self):
+        delta=time.time()-self.last_refresh
+        if not self.un_processed_items or delta>PERIODIC_REFRESH_CUTOFF:
+            self.refresh_unprocessed()
+            self.last_refresh=time.time()
+
+    def add_to_processed(self, job_id):
+        self.under_process_items.remove(job_id)
+
+    def refresh_unprocessed(self, limit = BATCH_SIZE):  # TODO: Refactor me
+        objs=list(InputJobs.objects.exclude(status = PROCESSED).values_list('id', flat = True)[:BATCH_SIZE])
+        [self.put(job_id) for job_id in objs]
+        self.un_processed_items=objs
+
+    def pop(self):
+        self.periodic_refresh()
+        item = None
+        with self._check_lock:
+            if self.un_processed_items:
+                item=self.un_processed_items.pop(0)
+#           item = self.redis_object.lpop(self.redis_key) # not sure, I can use this functionality, it implies queue func
+        if item:
+            obj=InputJobs.objects.get(id = item)
+            item={'values':[obj.value1, obj.value2]}
+            return {'id':obj.id, 'item':item}
+
+    def get(self, key):
+        return self.redis_object.hgetall(key)
+
+    def set(self, key, val_dict):
+        self.redis_object.hmset(key, val_dict)
+
+    def delete_item(self, key):
+        self.redis_object.delete(key)
+
+    def put(self, job_id):
+        self.redis_object.rpush(self.redis_key, job_id)
+'''
+
+
+
