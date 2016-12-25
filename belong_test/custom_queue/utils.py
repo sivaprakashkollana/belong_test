4f8e7f9fd856b5595c7aa67c3029d3b30cae5978
siva <siva@compile.com>
2016-12-25 23:56:29 +0530

    added initial queue implementation for queue using only redis

--- belong_test/custom_queue/utils.py
+++ belong_test/custom_queue/utils.py
@@ -60,52 +60,43 @@ class SimpleQueueFromRedis(object):
 
 '''
 Using only Redis for storing, data and to generate unique keys, db was not involved, dropped this, cos it didn't function with multiconsumer
-import time
-import threading
-from client.models import InputJobs
-
-PROCESSED = 1
-PERIODIC_REFRESH_CUTOFF=120 # seconds
-BATCH_SIZE=50
+LAST_READ_REDIS_KEY='last_read'
+LAST_READ_DEFAULT=0
+LATEST_ADDED_REDIS_KEY='latest_added'
+LATEST_ADDED_DEFAULT=0
+PROCESSED=1
+PROCESSING=0
+IDLE=-1
 
 class SimpleQueueFromRedis(object):
     def __init__(self, redis_object):
         self.redis_object=redis_object
-        self.un_processed_items=[]
-        self.last_refresh=time.time()
-        self.redis_key='jobs'
         self.__init_data()
-        self._check_lock=threading.Lock()
 
     def __init_data(self):
-        self.redis_object.delete(self.redis_key)
-        self.refresh_unprocessed()
+        if not self.redis_object.exists(LAST_READ_REDIS_KEY):
+            self.set_lr_key(LAST_READ_DEFAULT)
+        if not self.redis_object.exists(LATEST_ADDED_REDIS_KEY):
+            self.set_la_key(LATEST_ADDED_DEFAULT)
 
-    def periodic_refresh(self):
-        delta=time.time()-self.last_refresh
-        if not self.un_processed_items or delta>PERIODIC_REFRESH_CUTOFF:
-            self.refresh_unprocessed()
-            self.last_refresh=time.time()
 
-    def add_to_processed(self, job_id):
-        self.under_process_items.remove(job_id)
+    def get_lr_key(self):
+        return int(self.get(LAST_READ_REDIS_KEY)[LAST_READ_REDIS_KEY])
 
-    def refresh_unprocessed(self, limit = BATCH_SIZE):  # TODO: Refactor me
-        objs=list(InputJobs.objects.exclude(status = PROCESSED).values_list('id', flat = True)[:BATCH_SIZE])
-        [self.put(job_id) for job_id in objs]
-        self.un_processed_items=objs
+    def set_lr_key(self, value):
+        self.set(LAST_READ_REDIS_KEY, {LAST_READ_REDIS_KEY:value})
+
+    def get_la_key(self):
+        return int(self.get(LATEST_ADDED_REDIS_KEY)[LATEST_ADDED_REDIS_KEY])
+
+    def set_la_key(self, value):
+        return self.set(LATEST_ADDED_REDIS_KEY, {LATEST_ADDED_REDIS_KEY:value})
 
     def pop(self):
-        self.periodic_refresh()
-        item = None
-        with self._check_lock:
-            if self.un_processed_items:
-                item=self.un_processed_items.pop(0)
-#           item = self.redis_object.lpop(self.redis_key) # not sure, I can use this functionality, it implies queue func
-        if item:
-            obj=InputJobs.objects.get(id = item)
-            item={'values':[obj.value1, obj.value2]}
-            return {'id':obj.id, 'item':item}
+        lr_key=self.get_lr_key()
+        if self.redis_object.exists(lr_key+1):
+            ret_item=self.get(lr_key+1)
+            return {'id':lr_key+1, 'item':ret_item}
 
     def get(self, key):
         return self.redis_object.hgetall(key)
@@ -116,9 +107,20 @@ class SimpleQueueFromRedis(object):
     def delete_item(self, key):
         self.redis_object.delete(key)
 
-    def put(self, job_id):
-        self.redis_object.rpush(self.redis_key, job_id)
-'''
+    def add(self, v_list):
+        new_key=self.get_la_key()+1
+        item={'values':v_list, 'status':IDLE, 'result':''}
+        self.set(new_key, item)
+        self.set_la_key(new_key)
+        return new_key
+
+    def update(self, key, result):
+        item=self.get(key)
+        item['result']=result
+        item['status']=PROCESSED
+        self.set(key, item)
+        self.set_lr_key(self.get_lr_key()+1)
+        return True'''
 
 
 
