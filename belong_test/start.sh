388f26e9e6eb15863fd9b4c440ab4846875eb03b
siva <siva@compile.com>
2016-12-26 19:09:41 +0530

    modified start.sh

--- belong_test/start.sh
+++ belong_test/start.sh
@@ -4,4 +4,4 @@ python manage.py runserver $JOB_SERVER  &
 python manage.py create_test_jobs --job_server="http://$JOB_SERVER" --workers=1 &
 python manage.py start_processing --job_server="http://$JOB_SERVER" --workers=1 &
 
-#python -m worker_app.tests $JOB_SERVER
+python -m worker_app.tests $JOB_SERVER
